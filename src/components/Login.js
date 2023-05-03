b487c1f01f1757a13957722e801dfef5d44d7544
ribamarneto <ribamar.sucupira@gmail.com>
2023-05-02 22:05:11 -0300

    adicionar login com telefone

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import { SocialIcon, Button } from '@rneui/themed'
+import { SocialIcon, FAB } from '@rneui/themed'
 import { GoogleSignin } from '@react-native-google-signin/google-signin';
 import auth from '@react-native-firebase/auth';
-import { View, Image } from 'react-native';
+import { View, Text } from 'react-native';
 import Home from './Home';
+import { Button } from '@rneui/base';
+import PhoneSignIn from './PhoneSignIn';
 
 GoogleSignin.configure({
   webClientId: '804973163973-o2fkl9cojfilghpjumfqb0k4bi99tgfp.apps.googleusercontent.com',
@@ -15,61 +17,64 @@ export default function Login() {
 
   // Handle user state changes
   function onAuthStateChanged(user) {
-      setUser(user);
-      if (initializing) setInitializing(false);
+    setUser(user);
+    if (initializing) setInitializing(false);
   }
 
   React.useEffect(() => {
-      const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
-      return subscriber; // unsubscribe on unmount
+    const subscriber = auth().onAuthStateChanged(onAuthStateChanged);
+    return subscriber; // unsubscribe on unmount
   }, []);
 
   if (initializing) return null;
 
   if (!user) {
     return (
-      <View style={{ alignItems: 'center'}}>
-                <SocialIcon
-                    title={'Entrar com Google'}
-                    onPress={() => onGoogleButtonPress().then(() => console.log('Signed in with Google!'))}
-                    type='google'
-                    button={true}
-                    style={{ width: 200}}                    
-                />
-                <SocialIcon
-                    title={'Entrar com Facebook'}
-                    onPress={{}}
-                    type='facebook'
-                    button={true}
-                    style={{ width: 200}}                    
-                />
+      <View style={{ alignItems: 'center' }}>
+        <SocialIcon
+          title={'Entrar com Google'}
+          onPress={() => onGoogleButtonPress().then(() => console.log('Signed in with Google!'))}
+          type='google'
+          button={true}
+          style={{ width: 200 }}
+        />
+        <PhoneSignIn/>
       </View>
-      
+
     );
   }
-  
+
   async function onGoogleButtonPress() {
     try {
       await GoogleSignin.hasPlayServices({ showPlayServicesUpdateDialog: true });
       // Get the users ID token
       const { idToken } = await GoogleSignin.signIn();
-  
+
       // Create a Google credential with the token
       const googleCredential = auth.GoogleAuthProvider.credential(idToken);
-  
+
       console.log("Credential: " + googleCredential)
-  
+
       // Sign-in the user with the credential
       return auth().signInWithCredential(googleCredential);
-  
+
     } catch (error) {
       throw (error);
     }
     // Check if your device supports Google Play
-  
+
   }
 
   return (
-      <Home/>
+    <View style={{ alignItems: 'center', paddingVertical: 250}}>
+        <SocialIcon
+          title={'Entrar com Google'}
+          onPress={() => onGoogleButtonPress().then(() => console.log('Signed in with Google!'))}
+          type='google'
+          button={true}
+          style={{ width: 250}}
+        />
+        <PhoneSignIn/>
+      </View>
   );
 }
